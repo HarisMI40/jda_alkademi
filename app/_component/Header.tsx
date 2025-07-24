5adfabdcaf874645dc54a12b22eebd1ce0f1a202
HarisMI40 <harismaulanaikhsan40@gmail.com>
2025-07-24 21:50:49 +0700

    Refactor Header component to use Link for navigation

--- app/_component/Header.tsx
+++ app/_component/Header.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center">
-      <a className="flex items-center justify-center" href="#">
+      <Link className="flex items-center justify-center" href="/">
         <BookOpenCheckIcon className="h-6 w-6" />
         <span className="sr-only">EduQuiz</span>
-      </a>
+      </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
         <Button variant="ghost">
           <Link href={"/login"}>Masuk</Link>
