1020f846573d39ea72822322d9cb21d9909c8363
HarisMI40 <harismaulanaikhsan40@gmail.com>
2025-07-26 16:54:50 +0700

    fix(header): prevent navbar display on login and register pages

--- app/_component/_header/Header.tsx
+++ app/_component/_header/Header.tsx
@@ -5,9 +5,16 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import SectionUserLogged from "./SectionUserLogged";
 import SectionUserNotLogin from "./SectionUserNotLogin";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
   const { data: session, status } = useSession();
+
+  const path = usePathname();
+
+  // jika ada di halaman login atau register, maka navbar tidak akan di tampilkan
+  if(path === "/login" || path === "/register") return;
+
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center">
       <Link className="flex items-center justify-center" href="/">
