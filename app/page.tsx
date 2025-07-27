7061a30475baa066898dd5a74745f3703c894a1e
HarisMI40 <harismaulanaikhsan40@gmail.com>
2025-07-27 09:25:16 +0700

    fix(page): add padding to main section for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       
-      <main className="flex-1">
+      <main className="flex-1 px-6">
         <HeroSection />
         <KuisSection />
         <FeaturesSection />
