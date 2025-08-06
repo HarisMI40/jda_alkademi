e02bfa52d1f0bf84d2bda71de41668138a8b7680
HarisMI40 <harismaulanaikhsan40@gmail.com>
2025-08-06 21:18:24 +0700

    Refactor: streamline Kuis component and enhance data fetching

--- app/(user)/kuis/[id]/page.tsx
+++ app/(user)/kuis/[id]/page.tsx
@@ -1,42 +1,50 @@
 import React from 'react'
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import Image from 'next/image';
+import axios from 'axios';
+import Link from 'next/link';
+import { Quiz } from '@/type/formQuestion';
 
 interface KuisPageProps {
   params: Promise<{ id: string }>
+}
+
+interface Tag {
+  id: number;
+  name: string;
+}
+
+type QuizWithTag = Quiz & {
+  tag: Tag; // Kuis bisa saja tidak memiliki tag
+};
+
+const showKuis = async (id: number) => {
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/kuis/${id}?view=summary`);
+  return response.data;
 }
 
 const Kuis = async (props: KuisPageProps) => {
   const params = await props.params;
   const { id } = params;
 
-  const image = {
-    'matematika-dasar' : '/matematika-dasar.png',
-    'fisika-dasar' : '/fisika-dasar.jpg',
-    'bahasa-inggris-dasar' : '/inggris-dasar.png'
-  };
+  const kuis:QuizWithTag = await showKuis(parseInt(id));
 
   return (
-    <div className="flex justify-center items-center min-h-[60vh]">
-      <Card className="w-full max-w-md">
+    <div className="flex justify-center items-start min-h-[60vh]">
+      <Card className="w-full max-w-md mt-10">
         <CardHeader>
-          <div className='mb-5'>
-          <Image
-            src={image[id as keyof typeof image]}
-            alt={id}
-            width={240}
-            height={240}
-            priority
-          />
-          </div>
-          <CardTitle className="capitalize text-2xl">{id.replace(/-/g, " ")}</CardTitle>
+          <CardTitle className="capitalize text-2xl">{kuis.title}</CardTitle>
+
+          <span className="mt-2 text-xs font-semibold bg-blue-100 text-blue-800 px-2 py-1 rounded-full w-max">
+            {kuis.tag.name}
+          </span>
         </CardHeader>
         <CardContent>
-          <p>Selamat datang di kuis <span className="font-semibold">{id.replace(/-/g, " ")}</span>. Klik tombol di bawah untuk memulai!</p>
+          <p>{kuis.description}. </p>
+          <p>Klik tombol di bawah untuk memulai!</p>
         </CardContent>
         <CardFooter>
-          <Button>Mulai</Button>
+          <Link href={`/kuis/${id}/play`}><Button>Mulai</Button></Link>
         </CardFooter>
       </Card>
     </div>
